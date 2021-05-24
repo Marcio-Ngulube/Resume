dcef83db6abb01836c2544f3cb34445384c84653
Marcio Ngulube <prorostmax@gmail.com>
2021-05-24 19:22:40 +0200

    first commit

---
+++ js/acessquant.js
@@ -0,0 +1,34 @@
+/*-----------------------------------------------------------------------------------
+/*
+/* Read file
+/*-----------------------------------------------------------------------------------*/
+
+ jQuery(document).ready(function($) {
+
+ //var filePath = 'https://v2.jokeapi.dev/joke/Any'; 
+
+var filePath = 'https://3.80.53.97:8443/access'; 
+
+var numberOfViews=1;
+
+ 
+$.getJSON(filePath, function( data ) {
+  /*$.each( data, function( key, val ) {
+    alert(val["setup"]);
+  });*/ 
+  //alert(data.acess);
+  numberOfViews=data.acess;
+  $("#counter").text("Acessed "+numberOfViews+" time(s)");
+		
+}); 
+
+ 
+}); 
+
+   
+
+
+/*-----------------------------------------------------------------------------------
+/*
+/* End of read file
+/*-----------------------------------------------------------------------------------*/
