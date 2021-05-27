617a83410ac80c23ee3feb90a2c846a6d61aa861
Marcio-Ngulube <ngulubemarcio@gmail.com>
2021-05-27 09:55:23 +0200

    Update runapp.sh

--- runapp.sh
+++ runapp.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 echo 'Starting Spring Boot app'
-ps -ef | grep ceevee-ssl-server-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs kill
 cd '/home/ubuntu/app/access'
 /usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar -Dspring.config.location=application.properties ceevee-ssl-server-0.0.1-SNAPSHOT.jar
