5c98b09dd5b1ec0893b2c5c8615cb877f5214766
Marcio Ngulube <prorostmax@gmail.com>
2021-05-30 00:01:34 +0200

    Update deployment process

--- stopapp.sh
+++ stopapp.sh
@@ -1,2 +1,12 @@
-#!/bin/bash
-ps -ef | grep ceevee-ssl-server-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs kill
+#!/usr/bin/env bash
+SERVICE='ceevee-ssl-server-0.0.1-SNAPSHOT.jar'
+ 
+if ps ax | grep -v grep | grep $SERVICE > /dev/null
+then
+    echo "$SERVICE service running, everything is fine"
+	ps -ef | grep ceevee-ssl-server-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs kill
+else
+    echo "$SERVICE is not running"
+    echo "$SERVICE is not running!" | mail -s "$SERVICE down" root
+fi
+
