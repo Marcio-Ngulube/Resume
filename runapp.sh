02e89d46cb22db8e57332a110880bf5c36be984c
Marcio-Ngulube <ngulubemarcio@gmail.com>
2021-05-30 00:13:40 +0200

    Continous deployment of Backend

--- runapp.sh
+++ runapp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo 'Starting Spring Boot app'
 cd '/home/ubuntu/app/access'
-/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar -Dspring.config.location=application.properties ceevee-ssl-server-0.0.1-SNAPSHOT.jar
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar -Dspring.config.location=application.properties ceevee-ssl-server-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
