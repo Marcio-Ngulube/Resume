b612944004b7f05a58875fb531dd1b66e7c9cbf6
Marcio-Ngulube <ngulubemarcio@gmail.com>
2021-06-27 22:47:44 +0200

    Update acessquant.js

--- js/acessquant.js
+++ js/acessquant.js
@@ -7,7 +7,7 @@
 
  //var filePath = 'https://v2.jokeapi.dev/joke/Any'; 
 
-var filePath = 'https://3.80.53.97:8443/access'; 
+var filePath = 'https://xo4ugng2li.execute-api.us-east-1.amazonaws.com/production/access'; 
 
 var numberOfViews=1;
 
